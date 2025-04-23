55e85b0e0690ec5a8c63b5f0df6dff6da461f91d
Mathieu B <mathieu@playgrounds.network>
2025-04-23 12:35:18 +0200

    fix: package.json scripts and run prettier

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
-import nextra from 'nextra';
+import nextra from "nextra";
 
 /** @type {import('next').NextConfig} */
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true,
 });
 
-export default withNextra(
-  {
-    output: 'export',
-    images: { unoptimized: true },
-    //basePath: '/rig-docs'
-  }
-);
+export default withNextra({
+  output: "export",
+  images: { unoptimized: true },
+  //basePath: '/rig-docs'
+});
